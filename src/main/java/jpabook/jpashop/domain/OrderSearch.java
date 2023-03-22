1063c1530774d15ea02a739ee3c7fd87ce927540
renechoi <renechoi90@gmail.com>
2023-03-22 18:45:24 +0900

    feat: implement search function

---
+++ src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain;
+
+public class OrderSearch {
+    private String memberName;
+    private OrderStatus orderStatus;
+
+
+    public Object getOrderStatus() {
+        return null;
+    }
+
+    public CharSequence getMemberName() {
+        return null;
+    }
+}
