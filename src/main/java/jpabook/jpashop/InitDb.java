e7293676eb216f1f3522e5347038e2585d1fa7ab
renechoi <renechoi90@gmail.com>
2023-03-23 11:49:34 +0900

    feat: entity exposure way

--- src/main/java/jpabook/jpashop/InitDb.java
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -5,9 +5,19 @@ import jpabook.jpashop.domain.item.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 
+/**
+ * 종 주문 2개
+ * * userA
+ * 	 * JPA1 BOOK
+ * 	 * JPA2 BOOK
+ * * userB
+ * 	 * SPRING1 BOOK
+ * 	 * SPRING2 BOOK
+ */
 @Component
 @RequiredArgsConstructor
 public class InitDb {
@@ -24,33 +34,43 @@ public class InitDb {
     @Transactional
     @RequiredArgsConstructor
     static class InitService {
+
         private final EntityManager em;
 
         public void dbInit1() {
+            System.out.println("Init1" + this.getClass());
             Member member = createMember("userA", "서울", "1", "1111");
             em.persist(member);
+
             Book book1 = createBook("JPA1 BOOK", 10000, 100);
             em.persist(book1);
+
             Book book2 = createBook("JPA2 BOOK", 20000, 100);
             em.persist(book2);
+
             OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
             OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
-            Order order = Order.createOrder(member, createDelivery(member), orderItem1, orderItem2);
+
+            Delivery delivery = createDelivery(member);
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
             em.persist(order);
         }
 
         public void dbInit2() {
             Member member = createMember("userB", "진주", "2", "2222");
             em.persist(member);
+
             Book book1 = createBook("SPRING1 BOOK", 20000, 200);
             em.persist(book1);
+
             Book book2 = createBook("SPRING2 BOOK", 40000, 300);
             em.persist(book2);
-            Delivery delivery = createDelivery(member);
+
             OrderItem orderItem1 = OrderItem.createOrderItem(book1, 20000, 3);
             OrderItem orderItem2 = OrderItem.createOrderItem(book2, 40000, 4);
-            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
 
+            Delivery delivery = createDelivery(member);
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
             em.persist(order);
         }
 
@@ -62,11 +82,11 @@ public class InitDb {
         }
 
         private Book createBook(String name, int price, int stockQuantity) {
-            Book book = new Book();
-            book.setName(name);
-            book.setPrice(price);
-            book.setStockQuantity(stockQuantity);
-            return book;
+            Book book1 = new Book();
+            book1.setName(name);
+            book1.setPrice(price);
+            book1.setStockQuantity(stockQuantity);
+            return book1;
         }
 
         private Delivery createDelivery(Member member) {
@@ -76,5 +96,4 @@ public class InitDb {
         }
     }
 }
-
 
---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.api;
+
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * xToOne(ManyToOne, OneToOne) 관계 최적화 * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기환 }
+        }
+        return all;
+    }
+}
